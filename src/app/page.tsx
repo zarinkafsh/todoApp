2b11746cacc2b646a9a91803a7397a3c0c50c22a
zarinkafsh <somayehzarinkafsh@gmail.com>
2024-11-22 15:57:41 +0330

    replaced styles to tailwind classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,16 @@ import {
   Container,
   Typography,
 } from "@mui/material";
-import {TodoCreate, TodoList} from "@/components";
+import {TodoCreate, TodoList} from "@/app/components";
 
 export default function Home() {
   return (
-    <Container maxWidth="md" sx={{py: 4}}>
-      <Card
-        sx={{
-          maxWidth: 800,
-          mx: "auto",
-          px: {xs: 2, md: 6},
-          py: {xs: 2, md: 4},
-          boxShadow: 3,
-          borderRadius: 2,
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
+    <Container maxWidth="md" className="py-4 m-auto text-center	">
+      <Card className="min-h-96 p-8 m-auto">
+        <Typography className="text-left text-gray-500" variant="h4" component="h1" gutterBottom>
           Todo List
         </Typography>
-        <Box sx={{mb: 3}}>
+        <Box className="mb-3">
           <TodoCreate/>
         </Box>
         <TodoList/>
