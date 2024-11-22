f3eac020376139ccc1acd0ebb25f687af5586882
zarinkafsh <somayehzarinkafsh@gmail.com>
2024-11-22 14:08:28 +0330

    moved type file in types dir

--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -9,4 +9,8 @@ interface TodoContextType {
   addTodo: (todo: string) => void;
   removeTodo: (id: number) => void;
   editTodo: (id: number, updatedText: string) => void;
+  toggleTodo: (id: number) => void;
+  setCurrentTodo: (todo: TodoType) => void;
+  currentTodo: TodoType | null;
+  loading: boolean;
 }
